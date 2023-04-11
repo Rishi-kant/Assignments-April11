7837768cd6b33867806ec924e4728cd9c10b43d6
Rishi-Kant <kant3397@gmail.com>
2023-04-11 18:32:21 +0530

    creating a table by  fetching data from API

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
+
+import { useEffect, useState } from 'react';
 import './App.css';
+import  axios from 'axios';
+//https://jsonplaceholder.typicode.com/posts
 
 function App() {
+  const[data,setData]=useState([])
+  console.log(data)
+  
+  async function FetchData(){
+  const users =await axios.get("https://jsonplaceholder.typicode.com/posts")
+  setData(users.data)
+  
+  }
+  useEffect(()=>{
+   FetchData()
+  },[])
+
+  function handleDelete(ind){
+    const newData= data.filter((ele,index)=>{
+      return ind !==index
+    })
+    setData(newData)
+      
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+       <table>
+        <thead>
+           <tr>
+             <th>Serial </th>
+             <th>User Id</th>
+             <th>Title</th>
+             <th>Body</th>
+             <th>Actions</th>
+           </tr>
+        </thead>
+
+        <tbody>
+          {
+            data.map((ele,ind)=>{
+              return(
+                <tr key={ele.id}>
+                  <td>{ind+1}</td>
+                  <td>{ele.id}</td>
+                  <td>{ele.title}</td>
+                  <td>{ele.body}</td>
+                   <td onClick={()=>{
+                    handleDelete(ind)
+                   }}>
+                    &#x274C;
+                    </td>
+                    
+                </tr>
+              )
+            })
+          }
+        </tbody>
+
+       </table>
     </div>
   );
 }
